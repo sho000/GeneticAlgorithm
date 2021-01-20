00938b502c07d42b7deb2b6c10c806cbb64600c5
sho000 <sho@000studio.com>
2021-01-21 00:29:17 +0900

    init

--- main.py
+++ main.py
@@ -3,25 +3,21 @@ import random
 from Generation.Shape import Shape
 
 # globals
-g_population = 50
-g_shapes = []
+population = 50
+shapes = []
 
-def main():
-    global g_population, g_shapes
-    # 初期個体生成
-    for i in range(g_population):
-        x = random.uniform(0,1000)
-        y = random.uniform(0,1000)
-        z = 0
-        shape = Shape([x,y,z])
-        g_shapes.append(shape)
+# 初期個体生成
+for i in range(population):
+    x = random.uniform(0,1000)
+    y = random.uniform(0,1000)
+    z = 0
+    shape = Shape([x,y,z])
+    shapes.append(shape)
 
-    # 初期個体描画
-    for shape in g_shapes:
-        objs = shape.draw()
+# 初期個体描画
+for shape in shapes:
+    objs = shape.draw()
 
 
 # ga = GeneticAlgorithm(g_population)
 
-if(__name__ == "__main__") :
-    main()
